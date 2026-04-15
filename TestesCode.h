c0d5f63b6928a32a585ed4ce8c4cc1965e1cc76d
Sergio Magalhaes <39863@ufp.efu.pt>
2026-04-15 12:14:29 +0100

    Testes are on a separate file .c

---
+++ TestesCode.h
@@ -0,0 +1,12 @@
+//
+// Created by clonic on 4/15/26.
+//
+
+#ifndef MULTIMEDIAPROJETO_TESTESCODE_H
+#define MULTIMEDIAPROJETO_TESTESCODE_H
+
+void Compress_Decompress_ALL(const char* comp_dir, const char* rest_dir);
+void Compress_Decompress_Especifico(const char* comp_dir, const char* rest_dir);
+void Teste_String_Manual(const char* comp_dir, const char* rest_dir);
+
+#endif //MULTIMEDIAPROJETO_TESTESCODE_H
